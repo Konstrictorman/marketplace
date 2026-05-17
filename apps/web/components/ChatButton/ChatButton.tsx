6194f3f277ec9e0cc70c41ccc55b758762ba3a97
Unwished <santiago.alvarez.2k7@gmail.com>
2026-05-17 11:40:07 -0500

    Implementation of chat

--- apps/web/components/ChatButton/ChatButton.tsx
+++ apps/web/components/ChatButton/ChatButton.tsx
@@ -1,10 +1,45 @@
 "use client";
 import ChatIcon from "@mui/icons-material/Chat";
-import { IconButton } from "@mui/material";
+import { IconButton, Button } from "@mui/material";
 
-export default function ChatButton() {
+type ChatButtonProps = {
+  onClick: () => void;
+  variant?: "navbar" | "modal";
+};
+
+export default function ChatButton({
+  onClick,
+  variant = "navbar",
+}: ChatButtonProps) {
+  if (variant === "modal") {
+    return (
+      <Button
+        variant="outlined"
+        fullWidth
+        startIcon={<ChatIcon />}
+        onClick={onClick}
+        sx={{
+          textTransform: "none",
+          borderRadius: "10px",
+          py: 1.2,
+          borderColor: "rgb(24,62,157)",
+          color: "rgb(24,62,157)",
+          "&:hover": {
+            borderColor: "rgb(29, 54, 120)",
+            backgroundColor: "rgba(24, 62, 157, 0.05",
+          },
+        }}
+      >
+        Chat with seller
+      </Button>
+    );
+  }
   return (
-    <IconButton size="small" sx={{ color: "rgb(255, 255, 255)" }}>
+    <IconButton
+      size="small"
+      onClick={onClick}
+      sx={{ color: "rgb(255, 255, 255)" }}
+    >
       <ChatIcon />
     </IconButton>
   );
